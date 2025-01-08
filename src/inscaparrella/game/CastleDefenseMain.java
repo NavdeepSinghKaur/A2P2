1790eca28edc65b6ef381ef71b70357e03a704f3
Navdeep Singh Kaur <98622361+NavdeepSinghKaur@users.noreply.github.com>
2025-01-08 20:55:40 +0100

    Minor changes

--- src/inscaparrella/game/CastleDefenseMain.java
+++ src/inscaparrella/game/CastleDefenseMain.java
@@ -1,14 +1,37 @@
 package inscaparrella.game;
-
 /*
 * Authors:
 *   - Navdeep Singh Kaur
 *   -
-*   - 
+*   -
 * */
 
+import java.util.Scanner;
+
 public class CastleDefenseMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static int difficultyMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println
+                ("~~~~ MEN Ú ~~~~\n" +
+                        "0. Sortir\n" +
+                        "1. Jugar en mode fàcil\n" +
+                        "2. Jugar en mode normal\n" +
+                        "3. Jugar en mode difı́cil"
+                );
+        System.out.print("Entrada: ");
+        return sc.nextInt();
     }
+
+    public static void board() {
+        
+    }
+
+    public static void main(String[] args) {
+        difficultyMenu();
+        sc.close();
+    }
+
 }
