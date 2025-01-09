cd006001cd7623d1db7a3c7f0194e271cefd1ea2
Navdeep Singh Kaur <98622361+NavdeepSinghKaur@users.noreply.github.com>
2025-01-09 17:20:13 +0100

    update

--- src/inscaparrella/game/BoardMain.java
+++ src/inscaparrella/game/BoardMain.java
@@ -1,5 +1,8 @@
 package inscaparrella.game;
+import java.sql.SQLOutput;
 import java.util.Random;
+import java.util.Scanner;
+
 public class BoardMain {
     //COLOR constants
     public static final String RESET = "\033[0m";          // Text Reset
@@ -13,32 +16,101 @@ public class BoardMain {
     public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
         int tableDimensions = random.nextInt(10, 15);
+        int kingPosition;
+        int towerX = 0;
+        int towerY;
+        boolean wrongTowerPositionX = true;
+        boolean wrongTowerPositionY = true;
+        String input;
 
+        // KING
+        kingPosition = (tableDimensions/2);
+
+        // Board
+        // WARNING: check if tower in same coordinate as king
+
+        // TOWER
+        System.out.println("Dimensions de la taula: " + tableDimensions);
+        while (wrongTowerPositionX) {
+            System.out.println("Fila torre: ");
+            towerX = sc.nextInt();
+            if (towerX == tableDimensions) {
+                System.out.println("La posició de la torre no pot estar als marges, torna a intentar-ho");
+            }
+            else if (towerX > tableDimensions || towerX < 0) {
+                System.out.println("Introdueix un número válid que estigui dins de les dimensions.");
+            }
+
+            else {
+                wrongTowerPositionX = false;
+            }
+        }
+
+        while (wrongTowerPositionY) {
+            System.out.println("Columna torre: ");
+            towerY = sc.nextInt();
+            if (towerY == tableDimensions) {
+                System.out.println("La posició de la torre no pot estar als marges, torna a intentar-ho");
+            }
+            else if (towerY > tableDimensions || towerY < 0) {
+                System.out.println("Introdueix un número válid que estigui dins de les dimensions.");
+            }
+            else {
+                wrongTowerPositionY = false;
+            }
+        }
+
+        System.out.println("Columna torre: ");
+        towerY = sc.nextInt();
+
+        // TABLE
         System.out.print("   ");
-        for(int i = 0; i <= 14; i++) {
+        for(int i = 0; i < tableDimensions; i++) {
             if (i <= 9) {
-                System.out.print(i + " ");
+                System.out.print(i + "  ");
             } else {
                 System.out.print(i + " ");
             }
         }
         System.out.println();
 
-        for(int i = 0; i <= 14; i++) {
+        for(int i = 0; i < tableDimensions; i++) {
             if (i <= 9) {
                 System.out.print(" " + i);
             } else {
                 System.out.print(i);
             }
 
-            // Print board contents
-            for(int j = 0; j <= 14; j++) {
-                System.out.print(" -");
-                
+            for(int j = 0; j < tableDimensions; j++) {
+                if (kingPosition-1 == j && kingPosition-1 == i) {
+                    System.out.print(GREEN_BOLD + " C " + RESET);
+                }
+                else if (towerX == i && towerY == j){
+                    System.out.print(YELLOW_BOLD + " T " + RESET);
+                }
+                else {
+                    System.out.print(" - ");
+                }
             }
             System.out.println();
         }
+
+        while (true) {
+            boolean borderOnX = random.nextBoolean();
+            boolean attackTower = random.nextBoolean();
+
+            int enemy
+
+            if(borderOnX){
+
+            }
+
+            System.out.println("Enemies");
+
+        }
+
     }
 }
